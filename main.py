31ac23bbf0c76932c0a2c3a5ef865305f5f30001
adwivedi <20mcmc06@uohyd.ac.in>
2022-04-14 16:43:58 +0530

    Branch File

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+int1=(int(input('Enter the first Number')))
+int2=(int(input('Enter the Second Number')))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(int1+ int2)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
